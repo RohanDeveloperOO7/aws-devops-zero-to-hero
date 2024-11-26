d576c4497549c2e83249e941fe2e4e58c6527c68
RohanDeveloperOO7 <140474100+RohanDeveloperOO7@users.noreply.github.com>
2024-11-26 11:46:25 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,4 +4,4 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull rrr123birhade/simple-python-flask-app
 # Run the Docker image as a container
-docker run -d -p 5000:5000 rrr123birhade/simple-python-flask-app
+docker run -d -p 8000:8000 rrr123birhade/simple-python-flask-app
