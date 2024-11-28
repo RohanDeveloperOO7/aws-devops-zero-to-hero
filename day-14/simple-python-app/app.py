4b479d5f1c2bbe5f5b19010805741b163ee56997
RohanDeveloperOO7 <140474100+RohanDeveloperOO7@users.noreply.github.com>
2024-11-28 15:59:10 +0530

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! My Name Is ROHAN and i am  aws engineer in pune'
+    return 'Hello, world! My Name Is ROHAN and i am  aws certified engineer in pune'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)  
